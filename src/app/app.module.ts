b65e1144f91a619070e25bbea1d8934c120c188b
Javier Alarcon <javifuche@gmail.com>
2023-01-30 19:18:51 +0100

    created app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SkeletonComponent
   ],
   imports: [
     BrowserModule,
